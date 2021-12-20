69ec219cbc206a7f35ecc3c809cc95b2c36f5832
Mark Fullmer <mfullmer@gmail.com>
2021-12-20 07:57:29 -0600

    Add license reference

--- phaser.js
+++ phaser.js
@@ -1,3 +1,10 @@
+/* 
+This code is licensed under GPL-3.0.
+
+Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. 
+
+Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
+*/
 function set() {
   var currentAge = 30;
   var partTimeYears = 5;
@@ -187,4 +194,3 @@ function fire() {
   document.getElementById("expensesDisplay").innerHTML = '$' + Number(expenses).toLocaleString();
   document.getElementById("returnRateDisplay").innerHTML = returnRate + '%';
 }
-
